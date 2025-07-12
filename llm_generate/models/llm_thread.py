8c13c109c17cbe36329c45455c26c565c8ab8ff3
Alexis Yushin <alexis@apexive.com>
2025-07-12 19:31:11 -0400

    Update llm_thread.py

--- llm_generate/models/llm_thread.py
+++ llm_generate/models/llm_thread.py
@@ -137,6 +137,9 @@ class LLMThread(models.Model):
             "type": "message_create",
             "message": generated_message.message_format()[0],
         }
+        
+        # Return the generated message for direct access
+        return generated_message
 
     @api.model
     def get_model_generation_io_by_id(self, model_id):
